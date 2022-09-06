af5a426b114d115771e428656f75f9ac9cfba1a0
ViktarYakimuk <ViktarYakimuk>
2022-09-07 01:06:16 +0300

    удаление второй цыфры из трехзначного числа.

---
+++ prz002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа. 
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 1000);
+int newnumber = 0;
+
+int NumberWithoutSecDig()
+{
+   newnumber = number / 100 * 10 + number % 10;
+   return newnumber;
+}
+
+NumberWithoutSecDig();
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Если из числа {number} удалить вторую цифру, получится число {newnumber}");
+System.Console.WriteLine();
